1c82534f3076c50fbd0371cf16725f762602a715
mer1s <merisahmatovic@gmail.com>
2024-01-24 02:11:46 +0100

    proradi

--- project/settings.py
+++ project/settings.py
@@ -98,8 +98,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'c*1Faca43gcGBFD612dbe-dc6*abB3FF',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '12722'
     }
 }
 
